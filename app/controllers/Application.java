efcad290429d285d3bede95a8b41a00448ca09e1
Jonathan <jonathan.mbg@hotmail.com>
2016-05-26 17:01:25 +0200

    Menu login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.utils.ErrorUtils;
 import play.*;
 import play.mvc.*;
 
@@ -13,4 +14,34 @@ public class Application extends Controller {
         render();
     }
 
+    public static void login() {
+        render();
+    }
+
+
+    public static void identifyUser(String login, String pswd) {
+        ErrorUtils retour = null;
+        Utilisateurs p = null;
+
+        try {
+            p = Utilisateurs.find("Select p " +
+                    "from Utilisateurs p " +
+                    "Where (p.login = ? " +
+                    "Or p.matricule = ? " +
+                    "Or p.email = ? " +
+                    ") And p.motDePasse = ?" +
+                    "", login, login, login, pswd).first();
+        } catch (Exception e) {
+            e.printStackTrace();
+            retour = ErrorUtils.createError(true, e.getMessage(), "erreur");
+        }
+
+        if (p == null) {
+            retour = ErrorUtils.createError(false, "Erreur dans la saisie de l'identifiant ou du mot de passe", "warn");
+        } else {
+            session.put("userId", p.id);
+        }
+
+        renderJSON(retour);
+    }
 }
